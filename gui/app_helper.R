89ac841e9c8c4a9126ce0d79004e88957081868a
jejoenje <jeroenminderman@gmail.com>
2018-04-18 14:18:02 +0100

    Started gui/app_helper.R

---
+++ gui/app_helper.R
@@ -0,0 +1,8 @@
+library(GMSE)
+source('notebook/goose_predict.R')
+
+input_name <- '~/Dropbox/Islay_goose_data_from_Tom_Jan_2018/Dataset/toy_data.csv'
+
+gmse_goose_multiplot(data_file=input_name)
+
+
