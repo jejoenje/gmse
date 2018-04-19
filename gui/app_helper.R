36e6e45114ebf0a74550b11e03fbda37b731cca4
jejoenje <jeroenminderman@gmail.com>
2018-04-19 13:30:24 +0100

    app_helper.R currently redundant

--- gui/app_helper.R
+++ gui/app_helper.R
@@ -3,6 +3,5 @@ source('notebook/goose_predict.R')
 
 input_name <- '~/Dropbox/Islay_goose_data_from_Tom_Jan_2018/Dataset/toy_data.csv'
 
-gmse_goose_multiplot(data_file=input_name)
-
+gmse_goose_multiplot(data_file=input_name, iterations=2, proj_yrs = 2, max_HB=1000, manage_target = 25000)
 
