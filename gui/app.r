6e8ea0adfaa51ffee4b34566237104f532e49f7d
jejoenje <jeroenminderman@gmail.com>
2018-04-25 17:44:01 +0100

    Added functionality to load selected input file and started text summary output generation

--- gui/app.r
+++ gui/app.r
@@ -1,14 +1,22 @@
 library(shiny)
-library(GMSE)
-source('notebook/goose_predict.R')
 
-input_name <- '~/Dropbox/Islay_goose_data_from_Tom_Jan_2018/Dataset/toy_data.csv'
+source('notebook/goose_predict.R')
+#rm(sims) # Housekeeping for testing
+
+### for testing only:
+# input <- list(input_name=data.frame(datapath=as.vector('~/Documents/toy_data.csv')), sims_in=2, yrs_in=2, maxHB_in=2000, target_in=25000)
+# input$input_name$datapath <- as.vector(input$input_name$datapath)
 
 progress_i <- 0
 assign("progress_i", progress_i, envir = globalenv())
 updateProgress <- function() {
     value <- progress$getValue()
     value <- value + (progress$getMax() - value) / 5
+}
+
+valInput <- function(val_filename) {
+    val_file <- load_input(val_filename)
+    return(validate_input(val_file))
 }
 
 
@@ -21,6 +29,12 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       
+      fileInput("input_name", "Choose data file (XLS, XLSX or CSV)",
+                accept = c("application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", ".xls", ".xlsx",
+                    "text/csv",
+                           "text/comma-separated-values,text/plain",
+                           ".csv")),
+        
       sliderInput("yrs_in", "Number of years to project:",
                   min = 1, max = 50,
                   value = 5),
@@ -32,7 +46,7 @@ ui <- fluidPage(
                   min = 5000, max = 50000,
                   value = 26000, step=1000),
       
-      sliderInput("maxHB_in", "Cull per year:",
+      sliderInput("maxHB_in", "Max. cull per year:",
                   min = 0, max = 10000, value = 2000, step = 1000),
     
       actionButton('run_in', 'Run simulations')
@@ -48,7 +62,7 @@ ui <- fluidPage(
                  p("The text below needs editing and expanding", style = "color:red; font-weight: bold"),
                  p("Goose-GMSE allows the user to easily run a series of population models for the Islay population of Greenland Barnacle Goose", 
                    em("Branta leucopsis.")),
-                 p("The models used here are based on logistic growth models where population carrying capacity and growth rate 
+                 p("The models used here are based on logistic growth models where po pulation carrying capacity and growth rate 
                    are functions of (i.e. constrained by) a number of predictor variables. In brief, these predictors are empirical data on (1) previous yearly 
                    winter goose counts (averages), (2) climatic variability in previous years in both Greenland and on Islay, (3) the available area of improved 
                    grassland on Islay in previous years, (4) the numbers of geese killed (hunted) on Iceland and Greenland."),
@@ -64,9 +78,24 @@ ui <- fluidPage(
                  br(),
                  actionButton('done', 'Go to output')
                  ),
+        tabPanel(title = "Help", value = "help_tab", 
+                 h3("Extra help for running Goose-GMSE simulations"),
+                 br(),
+                 strong("Key points to note:"),
+                 p(),
+                 tags$ul(
+                     tags$li("A base data (input) file", strong("must"), "be loaded and formatted correctly. The extact format details are as follows: XXX"),
+                     tags$li("The output graph and summarised output are the result of simulations including observation uncertainty and some process
+                             uncertainty regarding future environmental conditions. Thus, it should be noted that the output and any inferences from 
+                             them are stochastic (i.e. contain a degree of uncertainty).", strong("Thus, any interpretation and recommendations derived
+                             from this output should take account of this uncertainty: neither the projected population sizes nor the numbers culled should be 
+                             taken as absolute recommendation.")), 
+                     tags$li("Third list item")
+                 )
+                 ),
         tabPanel(title = "Output", value = "out_tab",
                     plotOutput('plot', height='500px'),
-                    textOutput('text_summary')
+                    htmlOutput('text_summary')
                  )
 
       )
@@ -77,7 +106,6 @@ ui <- fluidPage(
 
 server <- function(session, input, output) {
   
-  
   observeEvent(input$run_in, {
      updateTabsetPanel(session=session, inputId="inTabset", selected="out_tab")
     })
@@ -86,21 +114,51 @@ server <- function(session, input, output) {
 
   calcPlot <- eventReactive(input$run_in, {
     
+    validate(
+        need(try(input$input_name), "Please load a base data file")
+        #need(try(valInput(input$input_name$datapath)), 'File validation failed: please check formatting')
+    )
+    
     updateTabsetPanel(session=session, inputId="inTabset", selected="out_tab")
 
     progress_i <- 0
     assign("progress_i", progress_i, envir = globalenv())
-    progress$initialize(min = 0, max = input$sims_in*(input$yrs_in+1))
+    progress <- shiny::Progress$new(session = session, min = 0, max = input$sims_in*(input$yrs_in+1))
     on.exit(progress$close())
     assign("progress", progress, envir = globalenv())
     
     progress$set(message = "Running simulations...", value = 0)
     
-    sims <- gmse_goose_multiplot(data_file=input_name, 
+    sims <- gmse_goose_multiplot(
+                         data_file=input$input_name$datapath, 
                          iterations=input$sims_in, 
                          proj_yrs = input$yrs_in, 
                          max_HB=input$maxHB_in, 
                          manage_target = input$target_in)
+    assign("sims", sims, envir = globalenv())
+    
+  })
+  
+  genSummaryText <- eventReactive(input$run_in, {
+      validate(
+          need(try(sims), "Simulations not yet run")
+      )
+      res <- gmse_goose_summarise(sims)
+      orig_data <- goose_clean_data(input$input_name$datapath)
+      last_obs_yr <- max(orig_data$Year)
+      
+      tags$ul(
+          tags$li(paste("In", res$end_yr,"projected population size was between", floor(res$end_min), "and", floor(res$end_max), "individuals.") ),
+          tags$li(paste("The projected population in ", res$end_yr, 
+                        switch(as.character(res$end_min<input$target_in & res$end_max>input$target_in), 'TRUE'='does', 'FALSE'='does not'),
+                        "overlap with the population target of", input$target_in
+                        )),
+          tags$li(paste("After ", res$end_yr-last_obs_yr, "projected years, the mean population size is predicted to be", 
+                        floor(res$end_mean)-input$target_in, "individuals", 
+                        switch(as.character(sign(floor(res$end_mean)-input$target_in)), "-1"="below", "1"="above"), 
+                        "the population target of", input$target_in))
+      )
+      
 
   })
   
@@ -108,11 +166,8 @@ server <- function(session, input, output) {
     calcPlot()
   })
   
-  output$text_summary <- renderText({
-      "The summarised results are as follows:",
-      gmse_goose_summarise(sims)$end_yr
-      # p("In", gmse_goose_summarise(sims)$end_yr, ", the goose population is projected to be between",
-      #   floor(gmse_goose_summarise(sims)$end_min),"and", floor(gmse_goose_summarise(sims)$end_max)) 
+  output$text_summary <- renderPrint({
+    genSummaryText()
   })
   
 
